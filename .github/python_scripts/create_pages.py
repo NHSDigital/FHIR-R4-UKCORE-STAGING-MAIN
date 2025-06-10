40056105755c175f0a7aeb8d69d35b3e84a36770
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2025-06-10 11:13:26 +0100

    Update create_pages.py

--- .github/python_scripts/create_pages.py
+++ .github/python_scripts/create_pages.py
@@ -157,25 +157,4 @@ def create_profile_toc(path):
         return
 
 if __name__ == "__main__":
-    from check_assets_in_IG import FHIRAsset, list_ig_pages
-
-    asset = FHIRAsset('https://fhir.hl7.org.uk/StructureDefinition/UKCore-ServiceRequest','ServiceRequest', 'active', 'UKCore-ServiceRequest', 'UKCore-ServiceRequest.xml', 'http://hl7.org/fhir/StructureDefinition/ServiceRequest')
-
-    ig_folder =  "guides/UK-Core-Implementation-Guide-STU3-Sequence"
-    ig_path = './'+ig_folder+'/Home'
-    
-    '''!!!!!!!!! add str path to variables!!!!!!!!!!!!!!'''
-    extension_path = ig_path+'/ProfilesandExtensions/ExtensionLibrary'
-    profile_path = ig_path+'/ProfilesandExtensions'
-    codesystem_path = ig_path+'/Terminology/CodeSystems'
-    valueset_path = ig_path+'/Terminology/ValueSets'
-    #create_Profile_page(asset, profile_path)
-    asset.context = ['Condition', 'Observation']
-    #create_Extension_page(asset, extension_path)
-    #create_Terminology_page(asset, valueset_path, 'ValueSet')
-
-    #create_toc(codesystem_path)
-    #create_profile_toc(profile_path)
-
-
-    
+    pass
